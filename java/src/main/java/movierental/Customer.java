3c5c3180fae4be44a8305cfd0a3f23b1527dbf5b
Cyril Houillon <houillon.cyril@gmail.com>
2018-06-27 18:18:32 +0200

    add intermediary variable scoped to 1 rental for frenquent renter points calculation

--- java/src/main/java/movierental/Customer.java
+++ java/src/main/java/movierental/Customer.java
@@ -21,17 +21,20 @@ public class Customer {
 
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
+		int totalFrequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each: _rentals) {
             double thisAmount = each.amount();
 
+			int frequentRenterPoints = 0;
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
+
+			totalFrequentRenterPoints += frequentRenterPoints;
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -40,7 +43,7 @@ public class Customer {
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "You earned " + String.valueOf(totalFrequentRenterPoints) + " frequent renter points";
 
         return result;
     }
